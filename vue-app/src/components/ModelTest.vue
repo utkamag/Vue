4c5d7088fee62c64abd17cc1a95b9b5550ee8df1
utkamag <92789705+utkamag@users.noreply.github.com>
2022-09-15 17:25:06 +0400

    update code

---
+++ vue-app/src/components/ModelTest.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="mt-10">Input</div>
+  <input @click="sendData" placeholder="Test" v-model="input">
+  <div>Инпут: {{input}}</div>
+  <div class="mt-10">Textarea</div>
+  <textarea placeholder="Введите данные" v-model="textArea"/>
+  <div>Данные textarea: {{textArea}}</div>
+</template>
+
+<script>
+export default {
+  name: "ModelTest",
+  data() {
+    return{
+      textArea: "",
+      input: "",
+      model: {
+        input: "",
+        database: "Test"
+      }
+    }
+  },
+  methods: {
+    sendData() {
+      this.$emit("update:model", this.model.database)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- vue-app/src/pages/MainPage.vue
+++ vue-app/src/pages/MainPage.vue
@@ -2,11 +2,22 @@
   <div class="mt-16">
     MainPage
   </div>
+  <ModelTest @update:model="model"/>
 </template>
 
 <script>
+import ModelTest from "@/components/ModelTest";
+
 export default {
-  name: "MainPage"
+  name: "MainPage",
+  components: {ModelTest},
+  methods: {
+    model(database) {
+      console.log(database)
+    }
+  }
+
+
 }
 </script>
 
